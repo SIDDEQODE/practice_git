bdd408214a02f4434117fadbd245b7cf201632ad
Siddharth Nahar <siddharthnahar68@gmail.com>
2022-10-18 13:26:12 +0530

    Siddharth Commit on f3

--- lib/app/module/login/login.dart
+++ lib/app/module/login/login.dart
@@ -6,7 +6,7 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('f1 f2'),
+      body: Text('f1 f2,f3(By Siddharth)'),
     );
     return Scaffold();
   }
